98db817334aa2275c3a215f4b57e0abb27a16391
Zachary Sterman <90649750+Zsterman@users.noreply.github.com>
2021-10-08 14:45:16 -0400

    Add files via upload
    updated working password validator function with input

--- password.js
+++ password.js
@@ -1,12 +1,30 @@
-console.log('welcome to the password validator tool!')
-console.log('enter a password to validate')
+const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants');
 
-function passwordValidator(password){
-    if(password.length >= 10){
-        console.log('Success')
-    } else {
-        console.log('failure')
+console.log('Welcome to password validator');
+
+const readline = require('readline').createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout,
     }
-}
+)
 
-passwordValidator("fjdksjdiejd")
+readline.question('what password would you like to validate?', password => {
+    String(password);
+    if(password.length >= 10){
+        HTMLFormControlsCollection.log('Congrats!');
+        readline.close();
+    } else {
+        console.log("failed, try again");
+        readline.question('what password would you like to validate?', password => {
+            String(password);
+            if(password.length >= 10){
+                console.log('congrats!');
+                readline.close();
+            } else {
+                console.log('failed again, you suck');
+                readline.close();
+            }
+        })
+    }
+})
